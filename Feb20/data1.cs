4262822da871e6effcb38799154c185e855f98b5
Suraj4324 <125561572+Suraj4324@users.noreply.github.com>
2023-02-20 07:48:59 -0500

    Data 1 Added..

---
+++ Feb20/data1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feb20
+{
+    class data1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("data1 written");
+        }
+    }
+}
