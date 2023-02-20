5b235fe10d3b7c2720a61893d3b92f8735546d85
Suraj4324 <125561572+Suraj4324@users.noreply.github.com>
2023-02-20 07:33:14 -0500

    Initial Code Commit
    Added Project Files

---
+++ Feb20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Feb20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
