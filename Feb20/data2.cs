abf21ca2b76da4d200c580cdd3bda17adb199b3e
Suraj4324 <125561572+Suraj4324@users.noreply.github.com>
2023-02-20 08:10:41 -0500

    data 2 will be added...
    for #2

---
+++ Feb20/data2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feb20
+{
+    class data2
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("data2.. written");
+
+            Console.WriteLine("Newly created... first issue...");
+        }
+    }
+}
